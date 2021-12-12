e1d76039c37a1fe89cfa1f9582b958ab22c30040
Ayenson, Mika <mika.ayenson@jhuapl.edu>
2021-12-12 16:28:38 -0500

    test vercel

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Welcome to August Eats! Site in Development.
     </h1>
   )
 }
