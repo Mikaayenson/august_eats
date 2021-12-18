e37d8284c64ec15beabb264d499697fec3ee60b3
Mika Ayenson <mika.d.ayenson@gmail.com>
2021-12-18 13:28:38 -0500

    add seo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import LandingPage from "../components/landingPage";
+import Seo from "../components/seo"
 
 export default function Home() {
   return (
     <div>
+      <Seo/>
       <LandingPage/>
     </div>
   )
